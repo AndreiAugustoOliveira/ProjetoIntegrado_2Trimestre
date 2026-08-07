d85f7a9b79f5255c9c997463f5ca27ec68875f96
AndreiAugustoOliveira <andreifornalha@gmail.com>
2026-08-07 10:15:47 -0300

    Primeiro commit com IA e projeto em Cs inicialmente feitos

---
+++ Comunicacao_ProjetoIntegrado/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text.Json;
+using System.Net.Http;
+using System.Text;
+
+
+namespace ProjetoIntegrado
+{
+
+
+    class Program
+    {
+    
+        static void Main()
+        {
+           SerialPort porta = new SerialPort("COMX", 115200); 
+           porta.Open();
+
+          HttpClient client = new HttpClient();
+
+          while(true)
+            {
+                string linha = porta.ReadLine();
+                int valor = int.Parse(linha);
+
+                Console.WriteLine("Valor recebido: " + valor);
+
+                string json = "{" + "\"valor\":" + valor + "}";
+                var conteudo = new StringContent(json, Encoding.UTF8, "application/json");
+                var resposta = client.PostAsync("http://localhost:3000/leituras", conteudo).Result;
+
+                Console.WriteLine("Status: " + resposta.StatusCode);
+            }
+           
+        }
+
+        
+    }
+ }
